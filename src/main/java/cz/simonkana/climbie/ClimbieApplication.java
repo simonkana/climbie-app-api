b54ab0f8ab14a25068af79fb8cf53ecd5f900e7e
simonkana <simon.kana@seznam.cz>
2025-12-13 08:54:00 +0100

    Initial Spring Boot project config

---
+++ src/main/java/cz/simonkana/climbie/ClimbieApplication.java
@@ -0,0 +1,13 @@
+package cz.simonkana.climbie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimbieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/simonkana/climbie/ClimbieApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.simonkana.climbie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimbieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
